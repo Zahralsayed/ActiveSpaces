93377283312626d93843828a3a47708d6938acbe
aliHassanrabia <alihrabie@gmail.com>
2024-09-29 09:49:18 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-testing
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
+
+app.listen(PORT, () => {
+  console.log(`SpaceActive is running on PORT ${PORT}`)
+})
