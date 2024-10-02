04ecbccbc009f69ab8d272fc13cd6f4187ff0aca
FatemaJadeed <fatema.jadeed266@gmail.com>
2024-10-02 11:24:45 +0300

    createAdminUpdate

--- createAdmin.js
+++ createAdmin.js
@@ -1,28 +1,24 @@
-// scripts/createAdmin.js
 const mongoose = require('mongoose');
-const User = require('../models/User'); // تأكد من المسار الصحيح
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
-// الاتصال بقاعدة البيانات
 mongoose.connect('mongodb://localhost:27017/yourDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Database connected');
 
-        // إنشاء مستخدم جديد
         const adminUser = new User({
             username: 'admin',
             email: 'admin@example.com',
-            password: 'yourpassword', // تأكد من اختيار كلمة مرور آمنة
+            password: 'yourpassword',
             firstName: 'Admin',
             lastName: 'User',
-            role: ['Admin'] // دور المدير
+            role: ['Admin'] 
         });
 
-        // حفظ المستخدم في قاعدة البيانات
         adminUser.save()
             .then(() => {
                 console.log('Admin user created successfully');
-                mongoose.connection.close(); // اغلاق الاتصال بعد الانتهاء
+                mongoose.connection.close(); 
             })
             .catch(err => {
                 console.error('Error creating admin user:', err);
